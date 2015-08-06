3d25d2765d1761b3417c0f385af12e189b1c0ccf
macauley <macauleycheng@gmail.com>
2015-08-06 18:14:16 +0800

    change example

--- command.sh
+++ command.sh
@@ -1,1 +1,1 @@
-sudo ./oft --verbose  -i 1@eth0 -i 2@eth1 -i 3@eth3 --disable-ipv6 --host=192.168.2.4 --of-version=1.3 --port=6633 --test-dir=accton
+sudo ./oft --verbose  -i 1@eth1 -i 2@eth2 -i 3@eth3 --disable-ipv6 --host=192.168.2.4 --of-version=1.3 --port=6633 --test-dir=accton

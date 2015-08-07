d7157bbbd3a2dd09a94a5cab57fe07008939063e
macauley <macauleycheng@gmail.com>
2015-08-07 14:56:08 +0800

    add default timeout

--- command.sh
+++ command.sh
@@ -1,1 +1,1 @@
-sudo ./oft --verbose  -i 1@eth1 -i 2@eth2 -i 3@eth3 --disable-ipv6 --host=192.168.2.4 --of-version=1.3 --port=6633 --test-dir=accton
+sudo ./oft --verbose --default-timeout=5 -i 1@eth1 -i 2@eth2 -i 3@eth3 --disable-ipv6 --host=192.168.2.4 --of-version=1.3 --port=6633 --test-dir=accton

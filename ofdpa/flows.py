f2ff2d3e1bba2ea1864a1ac63830351a628b00a9
Flavio Castro <castro.flaviojr@gmail.com>
2016-02-03 14:22:14 -0500

    Reorganizing tests

--- ofdpa/flows.py
+++ ofdpa/flows.py
@@ -21,6 +21,7 @@ import ofp
 from oftest.testutils import *
 from accton_util import *
 
+@disabled
 class PacketInSrcMacMiss(base_tests.SimpleDataPlane):
     """
     Test packet in function on a src-mac miss
@@ -95,7 +96,7 @@ class PacketInUDP(base_tests.SimpleDataPlane):
             verify_packet_in(self, vlan_pkt, of_port, ofp.OFPR_ACTION)
 
             verify_no_other_packets(self)
-
+@disabled
 class PacketInIPTable(base_tests.SimpleDataPlane):
     """
     Test packet in function on IPTABLE
@@ -144,7 +145,7 @@ class PacketInIPTable(base_tests.SimpleDataPlane):
                 verify_packet_in(self, pkt, in_port, ofp.OFPR_ACTION)
                 #verify_no_other_packets(self)
 
-
+@disabled
 class ArpNL2(base_tests.SimpleDataPlane):
      def runTest(self):
         delete_all_flows(self.controller)
@@ -324,7 +325,7 @@ class L2FloodTagged(base_tests.SimpleDataPlane):
             verify_packets(self, pkt, tmp_ports)
 
         verify_no_other_packets(self)
-
+@disabled
 class L2FloodTaggedUnknownSrc(base_tests.SimpleDataPlane):
     """
     Test L2 flood to a vlan
@@ -438,7 +439,7 @@ class Mtu1500(base_tests.SimpleDataPlane):
 
                 verify_no_other_packets(self)
 
-
+@disabled
 class Mtu4000(base_tests.SimpleDataPlane):
     """
     Test output function for an exact-match flow
@@ -609,7 +610,7 @@ class L3VPNMPLS(base_tests.SimpleDataPlane):
                 verify_no_other_packets(self)
 
 
-class L3VPN_32(base_tests.SimpleDataPlane):
+class _32VPN(base_tests.SimpleDataPlane):
     """
             Insert IP packet
             Receive MPLS packet
@@ -640,13 +641,12 @@ class L3VPN_32(base_tests.SimpleDataPlane):
             #ecmp_msg=add_l3_ecmp_group(self.controller, vlan_id, [mpls_label_gid])
             do_barrier(self.controller)
             #add vlan flow table
-            add_one_vlan_table_flow(self.controller, port, vlan_id, vrf=2, flag=VLAN_TABLE_FLAG_ONLY_TAG)
+            add_one_vlan_table_flow(self.controller, port, vlan_id, vrf=0, flag=VLAN_TABLE_FLAG_ONLY_TAG)
             #add termination flow
             add_termination_flow(self.controller, port, 0x0800, intf_src_mac, vlan_id)
             #add routing flow
             dst_ip = dip + (vlan_id<<8)
-            add_unicast_routing_flow(self.controller, 0x0800, dst_ip, 0, mpls_gid, vrf=2)
-            #ecmp_msg.group_id, vrf=2)
+            add_unicast_routing_flow(self.controller, 0x0800, dst_ip, 0xffffffff, mpls_label_gid)
             #add entries in the Bridging table to avoid packet-in from mac learning
             group_id = encode_l2_interface_group_id(vlan_id, port)
             add_bridge_flow(self.controller, dst_mac, vlan_id, group_id, True)
@@ -674,8 +674,8 @@ class L3VPN_32(base_tests.SimpleDataPlane):
                 pkt=str(exp_pkt)
                 verify_packet(self, pkt, out_port)
                 verify_no_other_packets(self)
-
-class MplsTermination(base_tests.SimpleDataPlane):
+@disabled
+class UniqueMplsTermination(base_tests.SimpleDataPlane):
     """
 	Insert IP packet
 	Receive MPLS packet
@@ -1032,7 +1032,7 @@ class L3McastToVPN(base_tests.SimpleDataPlane):
         verify_packet(self, pkt, port2)
         verify_packet(self, pkt, port3)
         verify_no_other_packets(self)
-
+@disabled
 class LPM(base_tests.SimpleDataPlane):
     """
 	    Insert IP packet
